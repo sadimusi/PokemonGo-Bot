04c569a416a54f4eb79d12e86d1b20e2d7d3b8ea
Simon Marti <simon@ceilingcat.ch>
2016-08-17 10:01:04 +0200

    Added -miniv criteria modifier to PokemonOptimizer which favors pokemon meeting the minimum IV criteria

--- pokemongo_bot/cell_workers/pokemon_optimizer.py
+++ pokemongo_bot/cell_workers/pokemon_optimizer.py
@@ -212,7 +212,14 @@ class PokemonOptimizer(Datastore, BaseTask):
         return sorted(family, key=lambda p: self.get_rank(p, criteria), reverse=True)
 
     def get_rank(self, pokemon, criteria):
-        return tuple(getattr(pokemon, a, None) for a in criteria.get("sort"))
+        if criteria.endswith("-miniv"):
+            rank = self.get_rank(pokemon, criteria[:-6])
+            if pokemon.iv >= self.config_minimum_evolve_iv:
+                rank += 10000
+        else:
+            rank = tuple(getattr(pokemon, a, None) for a in criteria.get("sort"))
+
+        return rank
 
     def get_pokemon_max_cp(self, pokemon_name):
         return int(self.pokemon_max_cp.get(pokemon_name, 0))
